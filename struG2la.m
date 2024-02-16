0795085bf0ec5a2126fb5b95b362f5bf1fc0f5d0
xkmaxidian <xkma@xidian.edu.cn>
2024-02-16 17:03:40 +0800

    file move

--- struG2la.m
+++
@@ -1,7 +0,0 @@
-function label = struG2la(Z)
-    [n,m] = size(Z);
-    SS0 = sparse(n+m,n+m); SS0(1:n,n+1:end) = Z; SS0(n+1:end,1:n) = Z';
-    label = conncomp(graph(SS0));
-    label = label(1:n)';  
-
-end
